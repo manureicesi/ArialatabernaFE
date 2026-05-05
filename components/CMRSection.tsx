4c5cbe60da372357304cdd1da166f7bab13308ff
Manuel R <manuel.reices@cinfo.es>
2026-05-05 17:56:47 +0200

    Modificar image url en eventos

--- components/CMRSection.tsx
+++ components/CMRSection.tsx
@@ -800,7 +800,7 @@ const CMRSection: React.FC<CMRSectionProps> = ({
               dateEnd: it.dateEnd ?? null,
               timezone: it.timezone,
               locationName: it.locationName ?? null,
-              isPublished: it.isPublished,
+              isPublished: it.isPublished ?? false,
               imageUrl: `/api/v1/events/${it.id}/image`,
             };
           })
